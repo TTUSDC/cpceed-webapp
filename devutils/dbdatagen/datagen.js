c7fe986d559e05270bae0b7985a19c82ac12b247
asclines <injpk23@gmail.com>
2017-01-08 15:57:04 -0600

    Generate random email now

--- devutils/dbdatagen/datagen.js
+++ devutils/dbdatagen/datagen.js
@@ -4,12 +4,26 @@ var prompt = require('prompt') //Gets user input
 var tracer = require('tracer'); //For logging
 var admin = require("firebase-admin");
 
+
 //Logging stuff
 tracer.setLevel(4) //'log':0, 'trace':1, 'debug':2, 'info':3, 'warn':4, 'error':5
 var logger = tracer.console({
   format: "{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})",
   dateformat: "HH:MM:ss.L"
 });
+
+//Ever used Pythons String .format() method? This does the same thing
+String.prototype.format = function () {
+  var i = 0, args = arguments;
+  return this.replace(/{}/g, function () {
+    return typeof args[i] != 'undefined' ? args[i++] : '';
+  });
+};
+
+
+function genRandomString(){
+  return Math.random().toString(36).substring(7);
+}
 
 
 admin.initializeApp({
@@ -26,5 +40,20 @@ function onError(err) {
 
 //Generates count users
 function genUsers(count) {
-
+  admin.auth().createUser({
+    email: "user{}@example.com".format(genRandomString()),
+    emailVerified: true,
+    password: "secretPassword",
+    displayName: "Dummy User",
+    disabled: false
+  })
+    .then(function(userRecord) {
+      // A UserRecord representation of the newly created user is returned
+      console.log("Successfully created new user:", userRecord.uid);
+    })
+    .catch(function(error) {
+      console.log("Error creating new user:", error);
+    });
 }
+
+genUsers(1);
