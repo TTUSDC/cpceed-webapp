39c6490c22b91fff56bc498efdf2c1cff8d30dd5
asclines <injpk23@gmail.com>
2017-01-08 15:29:04 -0600

    Added prompt include and logging tools to datagen

--- devutils/dbdatagen/datagen.js
+++ devutils/dbdatagen/datagen.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
 var prompt = require('prompt') //Gets user input
+var tracer = require('tracer'); //For loggin
+
+//Logging stuff
+tracer.setLevel(4) //'log':0, 'trace':1, 'debug':2, 'info':3, 'warn':4, 'error':5
+var logger = tracer.console({
+  format: "{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})",
+  dateformat: "HH:MM:ss.L"
+});
+
+
 
 //Helper function for handling errors
 function onError(err) {
-    console.log(err);
-    return 1;
+  logger.error('Message: %s', err.message)
+  logger.debug('Stack: %j', err);
+  return 1;
 }
 
 
@@ -17,7 +28,7 @@ prompt.get({
   properties: {
 
   }
-}, function(err, result){
+}, function(err, result) {
   if(err) return onError(err);
 
 })
