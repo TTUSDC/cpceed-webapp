06f174b0b2c1ac1f4c7e8a2a1fd89f8aaccdcee7
NilsG-S <NilsG-S@users.noreply.github.com>
2017-01-08 16:50:33 -0600

    Modified NavBar.js to handle new routing scheme

--- src/app/components/NavBar/NavBar.js
+++ src/app/components/NavBar/NavBar.js
@@ -3,20 +3,58 @@ import AppBar from 'react-toolbox/lib/app_bar';
 import Navigation from 'react-toolbox/lib/navigation';
 import Button from 'react-toolbox/lib/button';
 import theme from './NavBar.scss';
+import AuthContainer from '../Auth/AuthContainer.js';
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            auth: false
+        };
+
+        this.renderAuth = this.renderAuth.bind(this);
+        this.authFinished = this.authFinished.bind(this);
+    }
+
+    renderAuth() {
+        this.setState({
+            auth: true
+        });
+    }
+
+    authFinished() {
+        this.setState({
+            auth: false
+        });
+    }
+
     render() {
+        var authView = null;
+        if(this.state.auth === true) {
+            authView = (
+                <AuthContainer
+                    authFinished={this.authFinished}
+                    authCancelled={this.authFinished} />
+            );
+        }
+
         return (
-            <AppBar theme={theme}>
-                <Navigation type='horizontal'>
-                    <Button label='Register' onClick={() => {
-                        this.props.navigate('register/');
-                    }}/>
-                    <Button label='Login' onClick={() => {
-                        this.props.navigate('login/');
-                    }}/>
-                </Navigation>
-            </AppBar>
+            <div>
+                <AppBar theme={theme}>
+                    <Navigation type='horizontal'>
+                        <Button label='Events' onClick={() => {
+                            this.props.navigate('events/');
+                        }}/>
+                        <Button label='Activity' onClick={() => {
+                            this.props.navigate('activity/');
+                        }}/>
+                        <Button label='Sign In' onClick={() => {
+                            this.renderAuth();
+                        }}/>
+                    </Navigation>
+                </AppBar>
+                {authView}
+            </div>
         );
     }
 }
