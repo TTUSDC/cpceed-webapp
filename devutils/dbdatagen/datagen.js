fadff3bbaeb7943940ab0a6af2d4804fc670a50d
asclines <injpk23@gmail.com>
2017-01-08 20:07:55 -0600

    Code cleanup, commenting and organization

--- devutils/dbdatagen/datagen.js
+++ devutils/dbdatagen/datagen.js
@@ -10,15 +10,8 @@ var colors = require("colors/safe"); //Makes user input pretty
 
 
 /*******************************************************************************
-* GLOBAL VARIABLES
-*******************************************************************************/
-var uidWriteStream;
-
-
-/*******************************************************************************
 * UTILS
 *******************************************************************************/
-
 //Logging stuff
 tracer.setLevel(0) //'log':0, 'trace':1, 'debug':2, 'info':3, 'warn':4, 'error':5
 var logger = tracer.console({
@@ -46,6 +39,7 @@ function onError(err) {
   return 1;
 }
 
+
 /*******************************************************************************
 * Firebase Initialization
 *******************************************************************************/
@@ -53,96 +47,15 @@ var app = admin.initializeApp({
   credential: admin.credential.cert("./cpceed-firebase-admin-key.json"),
   databaseURL: "https://cpceed.firebaseio.com"
 });
+
 var db = admin.database();
 
 
 
-//Type = student or admin
-function createProps(role) {
-  randomString = genRandomString();
-  props = {}
-  props.password = randomString;
-  props.user = {} //This is the object that will be placed in "/users/{uid}"
-  props.user.role = role;
-  props.user.email = "user{}@example.com".format(randomString)
-  props.user.firstName = "First"
-  props.user.lastName = "Last"
-  props.displayName = "dummyDisplay"
-
-  props.user = ((props.user.role == "student") ? generateStudentData(props.user) : props.user)
-  return props;
-}
-
-function generateStudentData(user) {
-  logger.log("Generating student data for", user.displayName)
-  user.approvalStatus = true;
-  user.studentId = genRandomString();
-  user.points = 10
-  return user;
-}
-
-
-function createUsers(propsList) {
-  var the_promises = [];
-  propsList.forEach(function(props) {
-    var deferred = Q.defer();
-    createUser(props, function(error, props) {
-      logger.log("Done creating user", props.uid);
-      uidWriteStream.write("{}\n".format(props.uid))
-      deferred.resolve(props)
-    })
-    the_promises.push(deferred.promise);
-  });
-  return Q.all(the_promises);
-}
-
-
-function createUser(props, cb) {
-  admin.auth().createUser({
-      email: props.user.email,
-      emailVerified: true,
-      password: props.password,
-      displayName: props.displayName,
-      disabled: false
-    })
-    .then(function(userRecord) {
-      // A UserRecord representation of the newly created user is returned
-      logger.log("Successfully created new user:", userRecord.uid);
-      props.uid = userRecord.uid;
-      var usersRef = db.ref("users/")
-      var userRef = usersRef.child(props.uid)
-      userRef.update(props.user, function(error) {
-        cb(error, props)
-      })
-
-    })
-    .catch(function(error) {
-      cb(error, props)
-    });
-}
-
-function deleteUser(uid, cb) {
-  admin.auth().deleteUser(uid)
-    .then(function() {
-      logger.log("Successfully deleted user", uid);
-      logger.log("Removing user data")
-      var usersRef = db.ref("users/")
-      usersRef.update({
-        [uid]: null
-      }, function(error) {
-        if(error) {
-          logger.warn("Error removing user data for UID:", uid, error)
-        }
-        logger.log("Removed user data")
-        cb(null, uid);
-      })
-    })
-    .catch(function(error) {
-      logger.error("Error deleting user:", error);
-      cb(error, uid);
-    });
-}
-
+/*******************************************************************************
+* Generating Data
+*******************************************************************************/
+var uidWriteStream; //Used to writing generated UIDs to file
 
 function generateData(){
   prompt.get({
@@ -168,7 +81,6 @@ function generateData(){
       for(var i = 0; i < result.studentCount; i++){
         propsList.push(createProps("student"))
       }
-
       for(var i = 0; i < result.adminCount; i++){
         propsList.push(createProps("admin"))
 
@@ -183,22 +95,90 @@ function generateData(){
           console.log("Error closing firebase app:", error);
         });
       })
-
   })
-
-
-
 }
 
-function deleteData(){
+//Type = student or admin
+function createProps(role) {
+  randomString = genRandomString();
+  props = {}
+  props.password = randomString;
+  props.user = {} //This is the object that will be placed in "/users/{uid}"
+  props.user.role = role;
+  props.user.email = "user{}@example.com".format(randomString)
+  props.user.firstName = "First"
+  props.user.lastName = "Last"
+  props.displayName = "dummyDisplay"
 
+  props.user = ((props.user.role == "student") ? generateStudentData(props.user) : props.user)
+  return props;
+}
+
+// Used to generate data specfic to the student role
+function generateStudentData(user) {
+  logger.log("Generating student data for", user.email)
+  user.approvalStatus = true;
+  user.studentId = genRandomString();
+  user.points = 10
+  return user;
+}
+
+// Calls #createUser on every props in the list and gathers all promises.
+// Returns a "promise of promises".
+function createUsers(propsList) {
+  var the_promises = [];
+  propsList.forEach(function(props) {
+    var deferred = Q.defer();
+    createUser(props, function(error, props) {
+      logger.log("Done creating user", props.uid);
+      uidWriteStream.write("{}\n".format(props.uid))
+      deferred.resolve(props)
+    })
+    the_promises.push(deferred.promise);
+  });
+  return Q.all(the_promises);
+}
+
+// Handles the firebase-admin calls to to creating the user and adding user data
+function createUser(props, cb) {
+  admin.auth().createUser({
+      email: props.user.email,
+      emailVerified: true,
+      password: props.password,
+      displayName: props.displayName,
+      disabled: false
+    })
+    .then(function(userRecord) {// A UserRecord representation of the newly created user is returned
+      logger.log("Successfully created new user:", userRecord.uid);
+      props.uid = userRecord.uid;
+      var usersRef = db.ref("users/")
+      var userRef = usersRef.child(props.uid)
+      userRef.update(props.user, function(error) {
+        cb(error, props)
+      })
+    })
+    .catch(function(error) {
+      cb(error, props)
+    });
+}
+
+
+/*******************************************************************************
+* Deleting Data
+*******************************************************************************/
+// Iterates through the passed in file to get the UIDs of people that need to be
+// deleted.
+function deleteData(uidFile){
   var uidList = []
   var delete_promises = [];
   logger.info("Deleting UIDs");
+
+  //Reads through the file in an async way
   var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./genUIDS')
+    input: require('fs').createReadStream(uidFile)
   });
 
+  //Called on each line read in by lineReader
   lineReader.on('line', function(line) {
     uidList.push(line);
     var deferred = Q.defer();
@@ -209,9 +189,10 @@ function deleteData(){
     delete_promises.push(deferred.promise);
   });
 
+  //Called when lineReader is done reading the file
   lineReader.on('close', function() {
     Q.all(delete_promises).then(function() {
-      fs.unlink('./genUIDS')
+      fs.unlink(uidFile)
       app.delete()
         .then(function() {
           logger.log("Firebase closed successfully");
@@ -221,12 +202,40 @@ function deleteData(){
         });
     })
   })
-
 }
-prompt.message = ("CPCEED Web App Database Data Generator")
+
+// Handles connecting to Firebase and deleting all relevant user data
+function deleteUser(uid, cb) {
+  admin.auth().deleteUser(uid)
+    .then(function() {
+      logger.log("Successfully deleted user", uid);
+      logger.log("Removing user data")
+      var usersRef = db.ref("users/")
+      usersRef.update({
+        [uid]: null
+      }, function(error) {
+        if(error) {
+          logger.warn("Error removing user data for UID:", uid, error)
+        }
+        logger.log("Removed user data")
+        cb(null, uid);
+      })
+    })
+    .catch(function(error) {
+      logger.error("Error deleting user:", error);
+      cb(error, uid);
+    });
+}
+
+
+/*******************************************************************************
+* User Input
+*******************************************************************************/
+prompt.message = ("DBDataGen")
 prompt.delimiter = colors.green(":");
 prompt.start();
 
+//Handles CLI arguemnts/options
 program
     .version('0.0.1')
     .option('-g, --gen', 'Generate data')
@@ -237,5 +246,5 @@ program
 if(program.gen){
   generateData();
 } else if(program.delete){
-  deleteData();
+  deleteData(program.delete);
 }
