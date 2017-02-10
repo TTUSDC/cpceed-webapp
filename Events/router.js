e3cf96142f71a440db08acaea1f7536694e617d0
Patrick <tone@thepat.xyz>
2017-02-09 22:07:55 -0600

    Created basic expressjs router.
    See User/ for example service structure.

---
+++ Events/router.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var eventRouter = express.Router();
+
+module.exports = { eventRouter };
---
+++ Reports/router.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var reportRouter = express.Router();
+
+module.exports = { reportRouter };
