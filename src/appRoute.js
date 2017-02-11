51e2a4f8b3b06e24bc42457f6de6ca1fff7327f0
NilsG-S <NilsG-S@users.noreply.github.com>
2017-02-10 20:21:21 -0600

    Removed dynamic loading from routes

--- src/appRoute.js
+++ src/appRoute.js
@@ -1,38 +1,20 @@
-const appRoute = {
-    /*
-        Each of the key-value pairs separated by commas represent a prop. To
-        add additional props, simply add another key-value pair. These props
-        can be accessed through this.props.route.prop (where prop is the key
-        in the new key-value pair) in the component that "getComponent" is
-        targeting.
-    */
+import React from 'react';
+import { Route, IndexRedirect } from 'react-router';
 
-    // Holds the value of the relative path from the parent route
-    path: '/',
+import MainContainer from './components/Main/MainContainer';
+import eventsRoute from './routes/Events';
+import activityRoute from './routes/Activity';
 
-    // Sets the primary component for this route
-    getComponent(nextState, callback) {
-        require.ensure([], (require) => {
-            callback(null, require('./components/Main/MainContainer').default)
-        })
-    },
+const appRoute = (
+    // Sets the path and primary component for this route
+    <Route path='/' component={MainContainer}>
+        {/* Load the events page by default */}
+        <IndexRedirect to='events' />
 
-    // Load the events page by default
-    getIndexRoute(partialNextState, callback) {
-        require.ensure([], (require) => {
-            callback(null, require('./routes/Events').default)
-        })
-    },
-
-    // Sets the children of the primary component
-    getChildRoutes(partialNextState, callback) {
-        require.ensure([], (require) => {
-            callback(null, [
-                require('./routes/Events').default,
-                require('./routes/Activity').default
-            ])
-        })
-    }
-};
+        {/* Set the children of the primary component */}
+        {eventsRoute}
+        {activityRoute}
+    </Route>
+);
 
 export default appRoute;
