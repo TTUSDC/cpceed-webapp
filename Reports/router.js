c443b4dc124fb094186276e42943cdfa7f9ea3ac
James Taber <jmtaber129@gmail.com>
2017-02-22 23:03:04 -0600

    Stubs for report POST and GET methods.
    Used to check the report schema.

--- Reports/router.js
+++ Reports/router.js
@@ -1,4 +1,38 @@
 var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017');
+var reportModels = require('./reportModels')
+var EventReport = reportModels.EventReport;
+var OtherReport = reportModels.OtherReport;
 var reportRouter = express.Router();
+
+reportRouter.post('/', (req, res) => {
+    var eventReport = new EventReport({
+        type: 'event',
+        approvalStatus: 'notApproved'
+    });
+    
+    eventReport.save(function(err, savedReport) {
+        if (err)
+            res.send(err);
+        
+        console.log({
+            message: 'report created',
+            id: savedReport.id
+        });
+        res.status(201).location(savedReport.id).end();
+    });
+    
+});
+
+reportRouter.get('/', (req, res) => {
+    var Report = mongoose.model('EventReport');
+    Report.find({}, function(err, reports) {
+        if (err) throw err;
+        
+        console.log(reports);
+        res.json(reports);
+    });
+})
 
 module.exports = { reportRouter };
