9377ca3a14c60c4fb3782813dc859558ba475411
NilsG-S <NilsG-S@users.noreply.github.com>
2017-02-26 02:20:13 -0600

    Conformed to new indentation standards

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -8,49 +8,49 @@ import Login from './Login.js';
 import Register from './Register.js';
 
 class Auth extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            index: 0
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+    };
 
-        this.handleTabChange = this.handleTabChange.bind(this);
-    }
+    this.handleTabChange = this.handleTabChange.bind(this);
+  }
 
-    handleTabChange(newIndex) {
-        this.setState({
-            index: newIndex
-        });
-    }
+  handleTabChange(newIndex) {
+    this.setState({
+      index: newIndex
+    });
+  }
 
-    render() {
-        return (
-            <Layer
-                closer={true}
-                flush={false}
-                align='center'
-                onClose={() => {
-                    this.props.authCancelled();
-                }}>
-                <Tabs
-                    activeIndex={this.state.index}
-                    justify='center'
-                    responsive={false}
-                    onActive={(event) => {
-                        this.handleTabChange(event);
-                    }}>
-                    <Tab title='Login'>
-                        <Login
-                            handleLogin={this.props.handleLogin} />
-                    </Tab>
-                    <Tab title='Register'>
-                        <Register
-                            handleRegister={this.props.handleRegister} />
-                    </Tab>
-                </Tabs>
-            </Layer>
-        );
-    }
+  render() {
+    return (
+      <Layer
+        closer={true}
+        flush={false}
+        align='center'
+        onClose={() => {
+          this.props.authCancelled();
+        }}>
+        <Tabs
+          activeIndex={this.state.index}
+          justify='center'
+          responsive={false}
+          onActive={(event) => {
+            this.handleTabChange(event);
+          }}>
+          <Tab title='Login'>
+            <Login
+              handleLogin={this.props.handleLogin} />
+          </Tab>
+          <Tab title='Register'>
+            <Register
+              handleRegister={this.props.handleRegister} />
+          </Tab>
+        </Tabs>
+      </Layer>
+    );
+  }
 }
 
 export default Auth;
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -7,124 +7,124 @@ import Footer from 'grommet/components/Footer';
 import Button from 'grommet/components/Button';
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            firstName: '',
-            lastName: '',
-            studentID: '',
-            role: 'student'
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      studentID: '',
+      role: 'student'
+    };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSelectChange = this.handleSelectChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event, key) {
+    this.setState({
+      [key]: event.target.value
+    });
+  }
+
+  handleSelectChange(event) {
+    this.setState({
+      role: event.option
+    });
+  }
+
+  handleSubmit(event) {
+    // This prevents a '?' from being appended to the URL
+    event.preventDefault();
+
+    var data = {
+      email: this.state.email,
+      password: this.state.password,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      studentID: this.state.studentID,
+      role: this.state.role
+    };
+
+    this.props.handleRegister(data);
+  }
+
+  render() {
+    var studentIDField = null;
+    if(this.state.role === 'student') {
+      studentIDField = (
+        <FormField label='Student ID'>
+          <input
+            type='text'
+            value={this.state.studentID}
+            onChange={(event) => {
+              this.handleInputChange(event, 'studentID');
+            }}/>
+        </FormField>
+      );
     }
 
-    handleInputChange(event, key) {
-        this.setState({
-            [key]: event.target.value
-        });
-    }
-
-    handleSelectChange(event) {
-        this.setState({
-            role: event.option
-        });
-    }
-
-    handleSubmit(event) {
-        // This prevents a '?' from being appended to the URL
-        event.preventDefault();
-
-        var data = {
-            email: this.state.email,
-            password: this.state.password,
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            studentID: this.state.studentID,
-            role: this.state.role
-        };
-
-        this.props.handleRegister(data);
-    }
-
-    render() {
-        var studentIDField = null;
-        if(this.state.role === 'student') {
-            studentIDField = (
-                <FormField label='Student ID'>
-                    <input
-                        type='text'
-                        value={this.state.studentID}
-                        onChange={(event) => {
-                            this.handleInputChange(event, 'studentID');
-                        }}/>
-                </FormField>
-            );
-        }
-
-        return (
-            <Form
-                pad='small'
-                plain={true}>
-                <fieldset>
-                    <FormField label='Role'>
-                        <Select
-                            options={['student', 'admin']}
-                            value={this.state.role}
-                            onChange={(event) => {
-                                this.handleSelectChange(event);
-                            }}/>
-                    </FormField>
-                    {studentIDField}
-                    <FormField label='First Name'>
-                        <input
-                            type='text'
-                            value={this.state.firstName}
-                            onChange={(event) => {
-                                this.handleInputChange(event, 'firstName');
-                            }}/>
-                    </FormField>
-                    <FormField label='Last Name'>
-                        <input
-                            type='text'
-                            value={this.state.lastName}
-                            onChange={(event) => {
-                                this.handleInputChange(event, 'lastName');
-                            }}/>
-                    </FormField>
-                    <FormField label='Email'>
-                        <input
-                            type='email'
-                            value={this.state.email}
-                            onChange={(event) => {
-                                this.handleInputChange(event, 'email');
-                            }}/>
-                    </FormField>
-                    <FormField label='Password'>
-                        <input
-                            type='password'
-                            value={this.state.password}
-                            onChange={(event) => {
-                                this.handleInputChange(event, 'password');
-                            }}/>
-                    </FormField>
-                </fieldset>
-                <Footer>
-                    <Button
-                        label='Register'
-                        type='submit'
-                        primary={true}
-                        onClick={(event) => {
-                            this.handleSubmit(event);
-                        }}/>
-                </Footer>
-            </Form>
-        );
-    }
+    return (
+      <Form
+        pad='small'
+        plain={true}>
+        <fieldset>
+          <FormField label='Role'>
+            <Select
+              options={['student', 'admin']}
+              value={this.state.role}
+              onChange={(event) => {
+                this.handleSelectChange(event);
+              }}/>
+          </FormField>
+          {studentIDField}
+          <FormField label='First Name'>
+            <input
+              type='text'
+              value={this.state.firstName}
+              onChange={(event) => {
+                this.handleInputChange(event, 'firstName');
+              }}/>
+          </FormField>
+          <FormField label='Last Name'>
+            <input
+              type='text'
+              value={this.state.lastName}
+              onChange={(event) => {
+                this.handleInputChange(event, 'lastName');
+              }}/>
+          </FormField>
+          <FormField label='Email'>
+            <input
+              type='email'
+              value={this.state.email}
+              onChange={(event) => {
+                this.handleInputChange(event, 'email');
+              }}/>
+          </FormField>
+          <FormField label='Password'>
+            <input
+              type='password'
+              value={this.state.password}
+              onChange={(event) => {
+                this.handleInputChange(event, 'password');
+              }}/>
+          </FormField>
+        </fieldset>
+        <Footer>
+          <Button
+            label='Register'
+            type='submit'
+            primary={true}
+            onClick={(event) => {
+              this.handleSubmit(event);
+            }}/>
+        </Footer>
+      </Form>
+    );
+  }
 }
 
 export default Register;
