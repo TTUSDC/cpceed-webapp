845d154e93abb7a872e4d16c1b3948ae79f10e5c
Alexander Clines <injpk23@gmail.com>
2017-02-27 15:58:15 -0600

    [server] Added dummy data

---
+++ src/server/server.js
@@ -0,0 +1,30 @@
+
+
+dummyData = {
+  users: {
+    "QveDEhTHWSgVbnL4NXrdD6rSvns1": {
+      email: "patty.lastname@ttu.edu",
+      firstName: "Patty",
+      lastName: "PattysLastName",
+      role: "ADMIN"
+    },
+    "hMXRXnHKQdbGmX9bwaZntRaJER03": {
+      approvalStatus: true,
+      email: "john.doesnt@ttu.edu",
+      firstName: "John",
+      lastName: "Doesnt",
+      points: 0,
+      role: "STUDENT",
+      studentId: "110452"
+    },
+    "k8fcP5IgEVcuVVaOTQWduYwheur1": {
+      approvalStatus: true,
+      email: "sally.does@ttu.edu",
+      firstName: "Sally",
+      lastName: "Does",
+      points: 0,
+      role: "STUDENT",
+      studentId: "110451"
+    }
+  }
+}
