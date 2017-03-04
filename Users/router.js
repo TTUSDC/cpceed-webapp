44f20289a266610be90ec72fe3c407992a27f2a7
RyanFaulkenberry100 <ryan.faulkenberry@ttu.edu>
2017-03-04 11:24:37 -0600

    Branch now adheres to style guide

--- Users/router.js
+++ Users/router.js
@@ -2,37 +2,37 @@ var express = require('express');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:80085');
 var userModels = require('./userModels');
-var StudentModel = userModels.Student;
-var AdminModel = userModels.Admin;
+var Student = userModels.Student;
+var Admin = userModels.Admin;
 var userService = require('./service.js');
 var userRouter = express.Router();
 
-// Create User
-userRouter.post('/:uid', (req, res) => {
-    var response = userService.createUser(req.body);
+// Create User.
+userRouter.post('/', (req, res) => {
+  var response = userService.createUser(req.body);
 
-    res.status(response.status).json(response.object);
+  res.status(response.status).json(response.object);
 });
 
-// Modify User
+// Modify User.
 userRouter.put('/:uid', (req, res) => {
-    var response = userService.modifyUser(req.params.uid, req.body);
+  var response = userService.modifyUser(req.params.uid, req.body);
 
-    res.status(response.status).json(response.body);
+  res.status(response.status).json(response.body);
 });
 
-// Delete User
+// Delete User.
 userRouter.delete('/:uid', (req, res) => {
-    var response = userService.deleteUser(req.params.uid);
+  var response = userService.deleteUser(req.params.uid);
 
-    res.status(response.status).json(response.object);
+  res.status(response.status).json(response.object);
 });
 
-// Get User
+// Get User.
 userRouter.get('/:uid', (req, res) => {
-    var response = userService.getUser(req.params.uid);
+  var response = userService.getUser(req.params.uid);
 
-    res.status(response.status).json(response.object);
+  res.status(response.status).json(response.object);
 });
 
 module.exports = { userRouter };
--- Users/service.js
+++ Users/service.js
@@ -1,63 +1,66 @@
 var response = require('../Objects/response.js');
+var userModels = require('./userModels');
+var Student = userModels.Student;
+var Admin = userModels.Admin;
 
 var createUser = (json) => {
-	// Create Student
-	if (json.role == 'Student') {
-		var Student = new StudentModel({
-			approvalStatus: json.approvalStatus,
-			email: json.email,
-			firstName: json.firstName,
-			lastName: json.lastName,
-			points: json.points,
-			role: json.role,
-			studentId: json.studentId
-		});
-	// TODO: Write new Student to database
-	}
+  if (json.role == 'Student') {
+    // Create new student.
+    var student = new Student({
+      approvalStatus: false,
+      email: json.email,
+      firstName: json.firstName,
+      lastName: json.lastName,
+      points: 0,
+      role: 'Student',
+      studentId: getUid();
+    });
+  // TODO(ryanfaulkenberry100): Write student to database.
 
-	// Create Admin
-	else if (json.role == 'Admin') {
-		var Admin = new AdminModel({
-			email: json.email,
-			firstName: json.firstName,
-			lastName: json.lastName,
-			role: json.role
-		});
-	// TODO: Write new Admin to database
-	}
+  } else if (json.role == 'Admin') {
+    // Create new admin.
+    var admin = new Admin({
+      email: json.email,
+      firstName: json.firstName,
+      lastName: json.lastName,
+      role: 'Admin'
+    });
+  // TODO(ryanfaulkenberry100): Write admin to database.
+
+  }
 
     return new response.ResponseObject(201, {"uid":"1", "url":"//www.google.com"});
 }
 
 var modifyUser = (id, json) => {
-	// Modify user as self
-    if (json.requesterUid == 0) {
-		modifyUserAsSelf(id, json);
-	}
-	// Modify user as admin
-	else if (json.jsonuesterUid == 1) {
-		modifyUserAsAdmin(id, json);
-	}
+  // TODO(ryanfaulkenberry100): Check if modifier is a user or an admin.
+  if () {
+    // Modify user as self.
+    modifyUserAsSelf(id, json);
+  } else if () {
+    // Modify user as admin.
+    modifyUserAsAdmin(id, json);
+  }
 
-    return new response.ResponseObject(200, {"url":"//www.google.com"});
+  return new response.ResponseObject(200, {"url":"//www.google.com"});
 }
 
 var deleteUser = (id) => {
 
-    // TODO: Delete a user and remove console.log
-    console.log(id);
+  // TODO(ryanfaulkenberry100): Delete a user and remove console.log.
+  console.log(id);
 
-    return new response.ResponseObject(200, {"url":"//www.google.com"});
+  return new response.ResponseObject(200, {"url":"//www.google.com"});
 }
 
 var getUser = (id) => {
-    // Returns a user
+  // Returns a user.
 
-    // TODO: Find the user
-    console.log(id);
+  // TODO(ryanfaulkenberry100): Find the user.
+  console.log(id);
 
-    return new response.ResponseObject(200, User);
+  return new response.ResponseObject(200, User);
 }
 
-// Define your public methods here
+// Define your public methods here.
 module.exports = { createUser, modifyUser, deleteUser, getUser };
