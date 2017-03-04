3935b32c8b2e97f78230b3c70ea376b82d58c8a9
Alexander Clines <injpk23@gmail.com>
2017-03-04 15:34:56 -0600

    [server] Formatting from PR requests

--- test/server/dummy-data.js
+++ test/server/dummy-data.js
@@ -1,55 +1,55 @@
 export let auth = {
-    "patty.lastname@ttu.edu": "patty123",
-    "john.doesnt@ttu.edu": "john234",
-    "sally.does@ttu.edu": "324sally"
-  }
+  "patty.lastname@ttu.edu": "patty123",
+  "john.doesnt@ttu.edu": "john234",
+  "sally.does@ttu.edu": "324sally"
+};
 
 export let emailToUid = {
-    "patty.lastname@ttu.edu": "QveDEhTHWSgVbnL4NXrdD6rSvns1",
-    "john.doesnt@ttu.edu": "hMXRXnHKQdbGmX9bwaZntRaJER03",
-    "sally.does@ttu.edu": "k8fcP5IgEVcuVVaOTQWduYwheur1"
-  }
+  "patty.lastname@ttu.edu": "QveDEhTHWSgVbnL4NXrdD6rSvns1",
+  "john.doesnt@ttu.edu": "hMXRXnHKQdbGmX9bwaZntRaJER03",
+  "sally.does@ttu.edu": "k8fcP5IgEVcuVVaOTQWduYwheur1"
+};
 
 export let users = {
-    "QveDEhTHWSgVbnL4NXrdD6rSvns1": {
-      email: "patty.lastname@ttu.edu",
-      firstName: "Patty",
-      lastName: "PattysLastName",
-      role: "ADMIN"
-    },
-    "hMXRXnHKQdbGmX9bwaZntRaJER03": {
-      approvalStatus: true,
-      email: "john.doesnt@ttu.edu",
-      firstName: "John",
-      lastName: "Doesnt",
-      points: 0,
-      role: "STUDENT",
-      studentId: "110452"
-    },
-    "k8fcP5IgEVcuVVaOTQWduYwheur1": {
-      approvalStatus: true,
-      email: "sally.does@ttu.edu",
-      firstName: "Sally",
-      lastName: "Does",
-      points: 0,
-      role: "STUDENT",
-      studentId: "110451"
-    }
+  "QveDEhTHWSgVbnL4NXrdD6rSvns1": {
+    email: "patty.lastname@ttu.edu",
+    firstName: "Patty",
+    lastName: "PattysLastName",
+    role: "ADMIN"
+  },
+  "hMXRXnHKQdbGmX9bwaZntRaJER03": {
+    approvalStatus: true,
+    email: "john.doesnt@ttu.edu",
+    firstName: "John",
+    lastName: "Doesnt",
+    points: 0,
+    role: "STUDENT",
+    studentId: "110452"
+  },
+  "k8fcP5IgEVcuVVaOTQWduYwheur1": {
+    approvalStatus: true,
+    email: "sally.does@ttu.edu",
+    firstName: "Sally",
+    lastName: "Does",
+    points: 0,
+    role: "STUDENT",
+    studentId: "110451"
   }
+};
 
 export let reports = {
-    "QdbGmX9bwaOTQWduYw": {
-      type: "event",
-      student: "110451",
-      event: "someevent"
-    }
+  "QdbGmX9bwaOTQWduYw": {
+    type: "event",
+    student: "110451",
+    event: "someevent"
   }
+};
 
 export let events = {
-    "QWduYwX": {
-      "datetime": "2017:05:08:07:00",
-      "location": "Silicon Valley",
-      "title": "GTC",
-      "description": "GPU Technology Conference. The Largest and most important event of the year for GPU developers."
-    }
+  "QWduYwX": {
+    "datetime": "2017:05:08:07:00",
+    "location": "Silicon Valley",
+    "title": "GTC",
+    "description": "GPU Technology Conference. The Largest and most important event of the year for GPU developers."
   }
+};
