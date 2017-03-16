b95c15ae0356fc1790e018d34bf9a40ef0a4552e
NilsG-S <NilsG-S@users.noreply.github.com>
2017-03-16 14:59:47 -0500

    Simplified Karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration
-// Generated on Wed Mar 15 2017 11:18:29 GMT-0500 (Central Daylight Time)
+var webpackConfig = require('./webpack/test.js');
 
 module.exports = function(config) {
   config.set({
@@ -7,56 +6,39 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-
 
     // list of files / patterns to load in the browser
     files: [
-      'build/test.bundle.js'
+      'test/index.js'
     ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
 
     // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/index.js': ['webpack']
     },
 
+    webpack: webpackConfig,
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // possible values: LOG_DISABLE, LOG_ERROR, LOG_WARN, LOG_INFO, LOG_DEBUG
     logLevel: config.LOG_INFO,
-
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-
     // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
--- webpack/test.js
+++ webpack/test.js
@@ -1,20 +1,9 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
-const path = require('path');
 
 const base = require('./base.js');
 
-const buildPath = path.resolve(__dirname, '../build');
-
 const test = webpackMerge(base, {
-  output: {
-    path: buildPath,
-    publicPath: '/',
-    filename: "test.bundle.js"
-  },
-
-  entry: './test/index.js',
-
   externals: {
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
