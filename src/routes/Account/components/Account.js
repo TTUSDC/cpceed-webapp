e583c33a6b6e06d5f6737f898c23d34d16ef4b37
NilsG-S <NilsG-S@users.noreply.github.com>
2017-03-20 14:31:03 -0500

    Added tabs to Accounts.js

--- src/routes/Account/components/Account.js
+++ src/routes/Account/components/Account.js
@@ -1,13 +1,44 @@
 import React from 'react';
 
+import Tabs from 'grommet/components/Tabs';
+import Tab from 'grommet/components/Tab';
+
 import RequireAuth from 'components/Auth/RequireAuth.js';
+import ProfileContainer from './ProfileContainer.js';
+import SecurityContainer from './SecurityContainer.js';
 
 class Account extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+    };
+
+    this.handleTabChange = this.handleTabChange.bind(this);
+  }
+
+  handleTabChange(newIndex) {
+    this.setState({
+      index: newIndex
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h2>Account</h2>
-      </div>
+      <Tabs
+        activeIndex={this.state.index}
+        justify='center'
+        responsive={false}
+        onActive={(event) => {
+          this.handleTabChange(event);
+        }}>
+        <Tab title='Profile'>
+          <ProfileContainer />
+        </Tab>
+        <Tab title='Security'>
+          <SecurityContainer />
+        </Tab>
+      </Tabs>
     );
   }
 }
