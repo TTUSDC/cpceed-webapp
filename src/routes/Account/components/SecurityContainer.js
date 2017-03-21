f1a6fc931af2d343ac933da121920077c3c5f973
NilsG-S <NilsG-S@users.noreply.github.com>
2017-03-20 19:06:24 -0500

    Implemented password update code

--- src/routes/Account/components/SecurityContainer.js
+++ src/routes/Account/components/SecurityContainer.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import * as firebase from 'firebase';
 
 import Security from './Security.js';
+import logger from 'logger/logger.js';
 
 class SecurityContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secErr: '',
+      waiting: false
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(password) {
+    this.setState({
+      waiting: true,
+      secErr: ''
+    });
+
+    var user = firebase.auth().currentUser;
+    user.updatePassword(password)
+      .then(() => {
+        logger.info('Password was changed');
+        this.setState({
+          waiting: false
+        });
+      })
+      .catch((e) => {
+        logger.error(e.message);
+        this.setState({
+          waiting: false,
+          secErr: e.message
+        });
+      });
+  }
+
   render() {
     return (
-      <Security />
+      <Security
+        handleSubmit={this.handleSubmit}
+        waiting={this.state.waiting}
+        secErr={this.state.secErr}/>
     );
   }
 }
