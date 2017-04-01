b5164c6caaf5a04e7e66063c2c7fb556c3742f5f
James Taber <jmtaber129@gmail.com>
2017-04-01 18:55:14 -0500

    Added tests for all reportManager methods.
    Also changed mockgoose version to one that supports Model.findById().  TODO(jmtaber129): Find a working mockgoose version that doesn't print mongoose.connect() arguments to the console.

--- test/report-manager_test.js
+++ test/report-manager_test.js
@@ -11,13 +11,15 @@ var OtherReport = reportModels.OtherReport;
 
 describe('reportManager', () => {
   beforeEach( (done) => {
-    mockgoose.reset();
-    done();
+    mongoose.connect('', () => {
+      mockgoose.reset();
+      done();
+    });
   });
   
   describe('#createReport', () => {
     
-    it('should call the callback with a created event report with correct fields', (done) => {
+    it('should call the callback with a created event report with correct fields and store report in DB', (done) => {
       var eventReport = {
         type: 'event',
         student: 'John Doe',
@@ -29,8 +31,7 @@ describe('reportManager', () => {
         assert.equal(createdReport.student, 'John Doe');
         assert.equal(createdReport.event, 'someEventUid');
         assert.equal(createdReport.approvalStatus, false);
-        done();
-        /*
+        
         Report.findById(createdReport.id, (err, report) => {
           assert.equal(err, null);
           assert.equal(report.type, 'EventReport');
@@ -39,7 +40,7 @@ describe('reportManager', () => {
           assert.equal(report.approvalStatus, false);
           done();
         });
-        */
+        
       });
     });
 
@@ -47,14 +48,51 @@ describe('reportManager', () => {
       var report = {
         type: 'invalidType',
         student: 'John Doe',
-      }
+      };
+      
       reportManager.createReport(report, {}, (err, createdReport) => {
         assert.equal(err.message, 'Invalid report type.');
         done();
       });
     });
   });
-  /*
+  
+  describe('#modifyReport', () => {
+    it('should modify the report', (done) => {
+      var originalReport = new EventReport({
+        student: 'John Doe',
+        event: 'someEventUid',
+      });
+      var updatedReportObject = {
+        student: 'Jane Doe',
+        approvalStatus: true,
+      };
+      originalReport.save((err, createdReport) => {
+        assert.equal(err, null);
+        reportManager.modifyReport(createdReport.id, updatedReportObject, {}, (err, updatedReport) => {
+          assert.equal(err, null);
+          done();
+        });
+      });
+    })
+  });
+  
+  describe('#deleteReport', () => {
+    it('should delete the report', (done) => {
+      var report = new EventReport({
+        student: 'John Doe',
+        event: 'someEventUid',
+      });
+      report.save((err, createdReport) => {
+        assert.equal(err, null);
+        reportManager.deleteReport(createdReport.id, {}, (err, deletedReport) => {
+          assert.equal(err, null);
+          done();
+        });
+      });
+    });
+  });
+  
   describe('#getReportById', () => {
     it('should call the callback with the report with the given id', (done) => {
       var report = new EventReport({
@@ -65,10 +103,33 @@ describe('reportManager', () => {
         assert.equal(err, null);
         reportManager.getReportById(createdReport.id, {}, (err, foundReport) => {
           assert.equal(err, null);
-          console.log(foundReport);
+          done();
         });
-      })
+      });
     });
   });
-  */
+  
+  describe('#getAllReports', () => {
+    it('should return all reports', (done) => {
+      var report1 = new EventReport({
+        student: 'John Doe',
+        event: 'someEventUid',
+      });
+      var report2 = new OtherReport({
+        student: 'Jane Doe',
+        title: 'Some report',
+      });
+      report1.save((err) => {
+        assert.equal(err, null);
+        report2.save((err) => {
+          assert.equal(err, null);
+          reportManager.getAllReports({}, {}, (err, reports) => {
+            assert.equal(err);
+            done();
+          })
+        });
+      });
+    })
+  });
+  
 });
