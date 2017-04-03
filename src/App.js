b11be0fc744945a1529f0442d05f3553e117c4f4
Alexander Clines <injpk23@gmail.com>
2017-04-03 14:31:21 -0500

    Removed firebase-config file as no longer needed.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-import config from './firebase-config.js';
-import appRoute from './app-route.js';
-import cpceedApp from 'redux/reducers.js';
-import logger from 'logger/logger.js';
-
 import init from 'server/server';
+import cpceedApp from 'redux/reducers.js';
+import appRoute from './app-route.js';
+// import logger from 'logger/logger.js';
 
 init();
 
 // store holds the redux store that allows app-wide state to be shared
 const store = createStore(cpceedApp);
-logger.info('SERVER', init);
-// logger.info('AUTH', server.auth);
+// logger.info('SERVER', init);
 const App = () => (
   // Provider shares store with components joined by connect()
   <Provider store={store}>
@@ -23,5 +19,5 @@ const App = () => (
   </Provider>
 );
 
-export {store};
+export { store };
 export default App;
