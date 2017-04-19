940a72617bf3627b10953ad61414b9549d755f7b
Alexander Clines <injpk23@gmail.com>
2017-04-19 10:23:16 -0500

    Added event callback and event creation documentation

--- src/events/event-manager.js
+++ src/events/event-manager.js
@@ -2,8 +2,25 @@ const eventModels = require('./event-models');
 
 const Event = eventModels.Event;
 
+/**
+ * Callback used by event CRUD methods that returns a single event.
+ * @callback eventCallback
+ * @param {Error} err - An error that occurred during the operation. Null if no
+ *    errors occurred.
+ * @param {Event} event - The event operated on.
+ */
 
+/**
+ * Creates a event using fields from the request object then calls back when
+ * the database save finishes.
+ * @param {Object} reqData - The request object containing the event data to
+ *   be created.
+ * @param {Object} locals - An object containg the current request's local
+ *   variables.
+ * @param {eventCallback} createCallback - Called once the operation finishes.
+ */
 const createEvent = (reqData, locals, createCallback) => {
+  // TODO(asclines): Add checks for required data and expection handling
   const event = new Event({
     creator: reqData.creator,
     category: reqData.category,
