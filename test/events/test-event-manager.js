64d6af673acfe04816e53a38565d1604c7b24928
Alexander Clines <injpk23@gmail.com>
2017-04-20 09:39:46 -0500

    Added unit testing for event manager #modifyEvent

--- test/events/test-event-manager.js
+++ test/events/test-event-manager.js
@@ -13,6 +13,18 @@ const Event = eventModels.Event;
 mockgoose(mongoose);
 
 describe('eventManager', () => {
+  const getDefaultTestEvent = () => {
+    const defaultTestEvent = {
+      creator: 'creatoruid123',
+      category: 'eventcategory',
+      datetime: '2017-04-19T20:34:00.000Z',
+      location: 'ECE 204',
+      title: 'Awesome Event',
+      description: 'This event has an awesome description',
+    };
+    return defaultTestEvent;
+  };
+
   before((done) => { mongoose.connect('', done); });
 
   beforeEach((done) => {
@@ -23,23 +35,16 @@ describe('eventManager', () => {
   after((done) => { mongoose.unmock(done); });
 
   describe('#createEvent', () => {
-    it('should pass a created event to the callback', (done) => {
-      const testEvent = {
-        creator: 'creatoruid123',
-        category: 'eventcategory',
-        datetime: '2017-04-19T20:34:00.000Z',
-        location: 'ECE 204',
-        title: 'Awesome Event',
-        description: 'This event has an awesome description',
-      };
+    it('should pass a created and saved event to the callback', (done) => {
+      const testEvent = getDefaultTestEvent();
       const testDatetime = testEvent.datetime;
       eventManager.createEvent(testEvent, {}, (createErr, createdEvent) => {
-        expect(createErr).to.be.null;
-        expect(createdEvent).to.not.be.null;
-        expect(createdEvent.datetime).to.be.sameMoment(testEvent.datetime);
         // The next line is to handle the fact that datetimes when equivalent don't
         // mean equal when compared.
         delete testEvent.datetime;
+        expect(createErr).to.be.null;
+        expect(createdEvent).to.not.be.null;
+        expect(createdEvent.datetime).to.be.sameMoment(testDatetime);
         expect(createdEvent).to.shallowDeepEqual(testEvent);
         Event.findById(createdEvent.id, (findErr, foundEvent) => {
           expect(findErr).to.be.null;
@@ -53,6 +58,39 @@ describe('eventManager', () => {
   });
 
   describe('#modifyEvent', () => {
-
+    it('should pass a modified and saved event to the callback', (done) => {
+      const originalEvent = new Event(getDefaultTestEvent());
+      const updatedEvent = {
+        creator: 'updatedCreatorId',
+        category: 'eventcategoryish',
+        datetime: '2017-05-19T20:34:00.000Z',
+        location: 'ECE 204',
+        title: 'Not Awesome Event',
+        description: 'This event had an awesome description',
+      };
+      const testDatetime = updatedEvent.datetime;
+      originalEvent.save((saveErr, createdEvent) => {
+        expect(saveErr).to.be.null;
+        expect(createdEvent).to.not.be.null;
+        eventManager.modifyEvent(createdEvent.id, updatedEvent, {},
+          (modifyErr, realUpdatedEvent) => {
+            // The next line is to handle the fact that datetimes when
+            // equivalent don't mean equal when compared.
+            delete updatedEvent.datetime;
+            expect(modifyErr).to.be.null;
+            expect(realUpdatedEvent).to.not.be.null;
+            expect(realUpdatedEvent.datetime)
+              .to.be.sameMoment(testDatetime);
+            expect(realUpdatedEvent).to.shallowDeepEqual(updatedEvent);
+            Event.findById(realUpdatedEvent.id, (findErr, foundEvent) => {
+              expect(findErr).to.be.null;
+              expect(foundEvent).to.not.be.null;
+              expect(foundEvent.datetime).to.be.sameMoment(testDatetime);
+              expect(foundEvent).to.shallowDeepEqual(updatedEvent);
+              done();
+            });
+          });
+      });
+    });
   });
 });
