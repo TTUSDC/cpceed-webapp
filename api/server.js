3ce49d86b0fc6dc853a7a1636bff6e9caf729183
Alexander Clines <injpk23@gmail.com>
2017-05-17 19:04:45 -0500

    Added new server file for routing, now express handles api and static

--- api/server.js
+++ api/server.js
@@ -1,65 +1,22 @@
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-const express = require('express');
+const express = require('express'); //saved
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
 // Routers.
 const auth = require('./auth/auth-router.js');
-const event = require('./events/router.js');
+//const event = require('./events/router.js');
 const report = require('./reports/report-router.js');
 const user = require('./users/user-router.js');
 
-// load environment variables from .env file.
-dotenv.load({ path: process.env.ENV_PATH || '.env.default' });
-
-// Express server.
-const app = express();
-
-// Express configuration.
-const port = process.env.PORT || 3000;
-app.set('port', port);
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Connect to mongodb.
-const mongoURL = process.env.MONGODB_URI || process.env.MONGOLAB_URI;
-mongoose.Promise = global.Promise;
-mongoose.connect(mongoURL);
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-  console.log('MongoDB connection error. Please make sure MongoDB is running.');
-  process.exit();
-});
 
 // App routes.
 const router = express.Router();
 router.use('/users', user.userRouter);
-router.use('/events', event.eventRouter);
+//router.use('/events', event.eventRouter);
 router.use('/reports', report.reportRouter);
 router.use('/auth', auth.router);
-app.use('/api', router);
 
-// 404.
-app.use((req, res, next) => {
-  const err = new Error('Route not found.');
 
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.send(err);
-});
-
-// Start express server.
-const server = app.listen(port, () => {
-  console.log('App is running at %d in %s mode', port, app.get('env'));
-  console.log('  Press ctrl-c to stop\n');
-});
-
-module.exports = server;
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const apiRouter = require('./api/server.js');
+
+// load environment variables from .env file.
+dotenv.load({ path: process.env.ENV_PATH || '.env.default' });
+
+// Express server.
+const app = express();
+
+// Express configuration.
+const port = process.env.PORT || 3000;
+app.set('port', port);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Connect to mongodb.
+const mongoURL = process.env.MONGODB_URI || process.env.MONGOLAB_URI;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURL);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('MongoDB connection error. Please make sure MongoDB is running.');
+  process.exit();
+});
+
+app.use('/', express.static(__dirname +'/build'));
+app.use('/api', apiRouter);
+
+// 404.
+app.use((req, res, next) => {
+  const err = new Error('Route not found.');
+
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+// Start express server.
+const server = app.listen(port, () => {
+  console.log('App is running at %d in %s mode', port, app.get('env'));
+  console.log('  Press ctrl-c to stop\n');
+});
+
+module.exports = server;
