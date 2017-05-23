5f8cde7a0ffca4b2806a4cc036c754582a8976e9
NilsG-S <NilsG-S@users.noreply.github.com>
2017-05-22 23:31:54 -0500

    Fixed eslint bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   "env": {
     "mocha": true,
     "node": true,
+    "browser": true,
   },
   "plugins": ["chai-friendly"],
   "rules": {
-    "no-unused-expressions": 0,
     "chai-friendly/no-unused-expressions": 2,
-    "browser": true,
     "import/no-extraneous-dependencies": 0,
     "import/extensions": 0,
     "import/no-unresolved": 0,
+    "jsx-quotes": [2, "prefer-single"],
   }
 };
