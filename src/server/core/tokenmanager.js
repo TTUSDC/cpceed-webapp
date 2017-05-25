a5b4838c27e464a726ea3d4393cc3c79816d33d0
Alexander Clines <injpk23@gmail.com>
2017-05-25 14:52:52 -0500

    Added testing for token manager

--- src/server/core/tokenmanager.js
+++ src/server/core/tokenmanager.js
@@ -1,13 +1,15 @@
+export const tokenKey = 'sessiontoken';
+
 export function decode(token) {
   const base64Url = token.split('.')[1];
   const base64 = base64Url.replace('-', '+').replace('_', '/');
   return JSON.parse(window.atob(base64));
 }
 
-export function saveToken(token){
-  localStorage.setItem('sessiontoken', token);
+export function saveToken(token) {
+  localStorage.setItem(tokenKey, token);
 }
 
-export function getToken(){
-  return localStorage.getItem('sessiontoken');
+export function getToken() {
+  return localStorage.getItem(tokenKey);
 }
---
+++ test/server/test-core-tokenmanager.js
@@ -0,0 +1,34 @@
+import * as tokenManager from 'server/core/tokenmanager';
+
+const expect = require('chai').expect;
+
+const testToken = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsZXhhbmRlckBjbGluZXMuY29tIiwicm9sZSI6IlN0dWRlbnQiLCJpc0FwcHJvdmVkIjpmYWxzZSwiaWF0IjoxNDk1NjYyNzY4fQ.SQToghIxuiszjQ0kB0L5WAKfvlMrRK4lkuLeT08Y9QMJDAvqaiGGVq4dzlqL1oXedHWt_-xihReyChbABiFskQ';
+
+export default describe('Server API: Core/tokenmanager', () => {
+  beforeEach(() => {
+    localStorage.removeItem(tokenManager.tokenKey);
+  });
+
+  describe('initial state', () => {
+    it('should not have a sessiontoken in localstorage', () => {
+      expect(localStorage.getItem(tokenManager.tokenKey)).to.be.null;
+    });
+  });
+
+  describe('#saveToken', () => {
+    it('should save the sessiontoken in localstorage', () => {
+      tokenManager.saveToken(testToken);
+      expect(localStorage.getItem(tokenManager.tokenKey)).to.equal(testToken);
+    });
+  });
+
+  describe('#getToken', () => {
+    it('should return the saved token', () => {
+      localStorage.setItem(tokenManager.tokenKey, testToken);
+      expect(tokenManager.getToken()).to.equal(testToken);
+    });
+    it('should return null', () => {
+      expect(tokenManager.getToken()).to.be.null;
+    });
+  });
+});
