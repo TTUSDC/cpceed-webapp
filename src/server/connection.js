a41928cac7190757a6f5164eae5eda5e36388941
Alexander Clines <injpk23@gmail.com>
2017-05-25 00:01:46 -0500

    Setup logging in for user, now need getUser before done

--- src/server/connection.js
+++ src/server/connection.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import logger from 'logger/logger';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api', //TODO(asclines): Move this to env
+  baseURL: 'http://localhost:3000/api', // TODO(asclines): Move this to env
   timeout: 1000,
 });
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export const post = (endpoint, data, onSuccess, onError) => {
-  instance.post(endpoint,data)
+  instance.post(endpoint, data)
     .then((res) => {
       logger.info(res.data, 'Recieved data');
       onSuccess(res.data);
@@ -16,7 +16,7 @@ export const post = (endpoint, data, onSuccess, onError) => {
     .catch((err) => {
       logger.error(err);
       onError(err);
-    })
-}
+    });
+};
 
 export default instance;
--- src/server/user-auth.js
+++ src/server/user-auth.js
@@ -1,22 +1,23 @@
 import { store } from 'App';
 import { updateUser, logoutUser } from 'redux/actions';
 import * as firebase from 'firebase';
-// import logger from 'logger/logger.js';
+import logger from 'logger/logger';
+import * as connection from 'server/connection';
 
 export function login(email, password) {
+  logger.info('Logging in');
   return new Promise((resolve, reject) => {
-    firebase.auth().signInWithEmailAndPassword(email, password).then((user) => {
-      const rootRef = firebase.database().ref();
-      const userRef = rootRef.child(`users/${user.uid}`);
-      userRef.once('value').then((snapshot) => {
-        const userData = snapshot.val();
-        userData.uid = user.uid;
-        store.dispatch(updateUser(userData));
-        resolve(snapshot.val());
-      });
-    }).catch((err) => {
-      reject(err);
-    });
+    const data = {
+      email: 'alexander@clines.com',
+      password: 'password1',
+    }
+    connection.post('/auth',data,
+    (res) => {
+      logger.info(`Then: ${res}`)
+      logger.error(res);
+      localStorage.setItem('sessiontoken', res.token); //localStorage.getItem('sessiontoken');
+      resolve();
+    },reject);
   });
 }
 
