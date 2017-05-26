3c5af765cdde38d3108d66d927002dc8e5fdd90a
Alexander Clines <injpk23@gmail.com>
2017-05-26 11:30:30 -0500

    Fixed createUser tests

--- test/api/users/test-user-manager.js
+++ test/api/users/test-user-manager.js
@@ -26,79 +26,81 @@ describe('userManager', () => {
 
   after((done) => { mongoose.unmock(done); });
 
-  describe('#createStudent', () => {
-    it('should pass a uid to the callback', (done) => {
-      const student = testUsers.student000;
+  describe('#createUser', () => {
+    describe('role: student', () => {
+      it('should pass a uid to the callback', (done) => {
+        const student = testUsers.student000;
 
-      userManager.createUser(student, (createErr, uid) => {
-        expect(createErr).to.be.null;
-        expect(uid).to.be.a('string');
+        userManager.createUser(student, (createErr, uid) => {
+          expect(createErr).to.be.null;
+          expect(uid).to.be.a('string');
 
-        Student.findById(uid, (studentErr, foundStudent) => {
-          expect(studentErr).to.be.null;
-          expect(foundStudent.isApproved).to.be.false;
-          expect(foundStudent.email).to.be.equal(student.email);
-          expect(foundStudent.name).to.be.equal(student.name);
-          expect(foundStudent.role).to.be.equal(student.role);
-          expect(foundStudent.studentId).to.be.equal(student.studentId);
-          foundStudent.comparePassword(student.password, (passwordErr, isMatch) => {
-            expect(isMatch).to.be.true;
+          Student.findById(uid, (studentErr, foundStudent) => {
+            expect(studentErr).to.be.null;
+            expect(foundStudent.isApproved).to.be.false;
+            expect(foundStudent.email).to.be.equal(student.email);
+            expect(foundStudent.name).to.be.equal(student.name);
+            expect(foundStudent.role).to.be.equal(student.role);
+            expect(foundStudent.studentId).to.be.equal(student.studentId);
+            foundStudent.comparePassword(student.password, (passwordErr, isMatch) => {
+              expect(isMatch).to.be.true;
+            });
+            done();
           });
+        });
+      });
+
+      it('should pass an error to the callback', (done) => {
+        const student = {
+          email: 'test@test.com',
+          nameame: 'Test',
+          role: 'student',
+          studentId: '',
+        };
+
+        userManager.createUser(student, (createErr, uid) => {
+          should.exist(createErr);
+          should.not.exist(uid);
           done();
         });
       });
     });
 
-    it('should pass an error to the callback', (done) => {
-      const student = {
-        email: 'test@test.com',
-        nameame: 'Test',
-        role: 'student',
-        studentId: '',
-      };
+    describe('role: admin', () => {
+      it('should pass a uid to the callback', (done) => {
+        const admin = testUsers.admin000;
 
-      userManager.createUser(student, (createErr, uid) => {
-        should.exist(createErr);
-        should.not.exist(uid);
-        done();
-      });
-    });
-  });
+        userManager.createUser(admin, (createErr, uid) => {
+          expect(createErr).to.be.null;
+          expect(uid).to.be.a('string');
 
-  describe('#createAdmin', () => {
-    it('should pass a uid to the callback', (done) => {
-      const admin = testUsers.admin000;
-
-      userManager.createUser(admin, (createErr, uid) => {
-        expect(createErr).to.be.null;
-        expect(uid).to.be.a('string');
-
-        Admin.findById(uid, (adminErr, foundAdmin) => {
-          expect(adminErr).to.be.null;
-          expect(foundAdmin.isApproved).to.be.false;
-          expect(foundAdmin.name).to.be.equal(admin.name);
-          expect(foundAdmin.email).to.be.equal(admin.email);
-          expect(foundAdmin.role).to.be.equal(admin.role);
-          foundAdmin.comparePassword(admin.password, (passwordErr, isMatch) => {
-            expect(isMatch).to.be.true;
+          Admin.findById(uid, (adminErr, foundAdmin) => {
+            expect(adminErr).to.be.null;
+            expect(foundAdmin.isApproved).to.be.false;
+            expect(foundAdmin.name).to.be.equal(admin.name);
+            expect(foundAdmin.email).to.be.equal(admin.email);
+            expect(foundAdmin.role).to.be.equal(admin.role);
+            foundAdmin.comparePassword(admin.password, (passwordErr, isMatch) => {
+              expect(isMatch).to.be.true;
+            });
+            done();
           });
-          done();
         });
       });
-    });
 
-    it('should pass an error to the callback', (done) => {
-      const admin = {
-        email: 'test@test.com',
-        name: 'Test',
-        role: 'student',
-        studentId: '',
-      };
+      it('should pass an error to the callback', (done) => {
+        const admin = {
+          email: 'test@test.com',
+          name: 'Test',
+          role: 'student',
+          studentId: '',
+        };
 
-      userManager.createUser(admin, (createErr, uid) => {
-        should.exist(createErr);
-        should.not.exist(uid);
-        done();
+        userManager.createUser(admin, (createErr, uid) => {
+          should.exist(createErr);
+          should.not.exist(uid);
+          done();
+        });
       });
     });
   });
