d998b6c1989891e17c8afcb66d586415f4e73b4a
Alexander Clines <injpk23@gmail.com>
2017-05-26 11:12:28 -0500

    Created new core folder with dummy users for testing

---
+++ test/core/users.js
@@ -0,0 +1,19 @@
+const student000 = {
+  email: 'student000@email.com',
+  password: 'P@ssw0rd!',
+  name: 'TestStudent000',
+  role: 'student',
+  studentId: '0001234',
+};
+
+const admin000 = {
+  email: 'admin000',
+  password: 'T0t@llyS3cure',
+  name: 'TestAdmin000',
+  role: 'admin',
+};
+
+module.exports = {
+  student000,
+  admin000,
+};
