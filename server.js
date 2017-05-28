ef479588c4b0ac1d406ed714212c8ad89ccab7ca
Alexander Clines <injpk23@gmail.com>
2017-05-28 16:24:20 -0500

    Fixed error catching

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.use((req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.send(err);
+  res.json(err.message);
 });
 
 // Start express server.
