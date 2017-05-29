bcbb2cfb4032aa5dee89dd86d6e9c8627d24105f
NilsG-S <NilsG-S@users.noreply.github.com>
2017-05-29 17:39:14 -0500

    Modified Settings route index.js files to conform to new way of routing

--- src/routes/Settings/routes/Account/index.js
+++ src/routes/Settings/routes/Account/index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import {Route} from 'react-router';
-
-import AccountContainer from './components/AccountContainer.js';
-
-const accountRoute = (
-  <Route path='account' component={AccountContainer} />
-);
-
-export default accountRoute;
+export { AccountContainer as default } from "./components/AccountContainer.jsx";
