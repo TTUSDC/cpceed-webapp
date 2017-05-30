9e079c312059101a8cd579f1b6d76423ad0c23f9
NilsG-S <NilsG-S@users.noreply.github.com>
2017-05-29 19:56:21 -0500

    Linted Auth.jsx and switched to prop-types

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,9 +1,11 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Layer from 'grommet/components/Layer';
 import Tabs from 'grommet/components/Tabs';
 import Tab from 'grommet/components/Tab';
 
+import logger from 'logger/logger.js';
 import Login from './Login.js';
 import Register from './Register.js';
 
@@ -11,7 +13,7 @@ class Auth extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      index: 0
+      index: 0,
     };
 
     this.handleTabChange = this.handleTabChange.bind(this);
@@ -19,37 +21,41 @@ class Auth extends React.Component {
 
   handleTabChange(newIndex) {
     this.setState({
-      index: newIndex
+      index: newIndex,
     });
   }
 
   render() {
     return (
       <Layer
-        closer={true}
+        closer
         flush={false}
         align='center'
         onClose={() => {
           this.props.authCancelled();
-        }}>
+        }}
+      >
         <Tabs
           activeIndex={this.state.index}
           justify='center'
           responsive={false}
           onActive={(event) => {
             this.handleTabChange(event);
-          }}>
+          }}
+        >
           <Tab title='Login'>
             <Login
               handleLogin={this.props.handleLogin}
               logErr={this.props.logErr}
-              waiting={this.props.waiting} />
+              waiting={this.props.waiting}
+            />
           </Tab>
           <Tab title='Register'>
             <Register
               handleRegister={this.props.handleRegister}
               regErr={this.props.regErr}
-              waiting={this.props.waiting} />
+              waiting={this.props.waiting}
+            />
           </Tab>
         </Tabs>
       </Layer>
@@ -60,16 +66,18 @@ class Auth extends React.Component {
 Auth.propTypes = {
   handleRegister: PropTypes.func,
   handleLogin: PropTypes.func,
-  authCancelled: PropTypes.func,
+  authCancelled: PropTypes.func.isRequired,
   regErr: PropTypes.string,
   logErr: PropTypes.string,
-  waiting: PropTypes.bool
+  waiting: PropTypes.bool,
 };
 
 Auth.defaultProps = {
+  handleRegister: () => logger.error('handleRegister called without being passed'),
+  handleLogin: () => logger.error('handleLogin called without being passed'),
   regErr: '',
   logErr: '',
-  waiting: false
+  waiting: false,
 };
 
 export default Auth;
