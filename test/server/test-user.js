08fe77821b8f4c4b11308c0606b87f4e3c649c61
Alexander Clines <alexander.clines@ttu.edu>
2017-06-17 11:15:46 -0500

    [server][test] Refactored test-user to use new Connection builder

--- test/server/test-user.js
+++ test/server/test-user.js
@@ -1,7 +1,6 @@
 import sinon from 'sinon';
 // import logger from 'logger/logger';
 import { createUser, deleteUser, modifyUser, getUser } from 'server/user';
-// import * as connection from 'server/core/connection';
 import Connection from 'server/core/connection';
 import * as tokenManager from 'server/core/tokenmanager';
 import { user38257001 as testUser } from './core/users';
@@ -16,11 +15,7 @@ const sandbox = sinon.sandbox.create();
 
 export default describe('Server API: User', () => {
   beforeEach(() => {
-    sandbox.stub(Connection, 'call');
-    // sandbox.stub(connection, 'post');
-    // sandbox.stub(connection, 'del');
-    // sandbox.stub(connection, 'put');
-    // sandbox.stub(connection, 'get');
+    sandbox.stub(Connection.prototype, 'call');
     sandbox.stub(tokenManager, 'getToken').callsFake(() => testToken.token);
     sandbox.spy(tokenManager, 'decode');
   });
@@ -33,18 +28,13 @@ export default describe('Server API: User', () => {
   describe('#createUser', () => {
     it('should create the user', (done) => {
       const expectedResult = { uid: '12345678' };
-      Connection.call.callsFake(() => {
-        console.log('YO YO YO');
+
+      Connection.prototype.call.callsFake((onSuccess) => {
         onSuccess(expectedResult);
       });
-      // connection.post.callsFake(
-      //   (endpoint, data, params, onSuccess) => {
-      //     expect(endpoint).to.equal('/users');
-      //     expect(data).to.equal(testUser);
-      //     onSuccess(expectedResult);
-      //   });
+
       createUser(testUser).then((data) => {
-        // expect(connection.post).to.have.been.calledOnce;
+        expect(Connection.prototype.call).to.have.been.calledOnce;
         expect(data).to.equal(expectedResult);
         done();
       }).catch(done);
@@ -53,19 +43,10 @@ export default describe('Server API: User', () => {
 
   describe('#deleteUser', () => {
     it('should delete the user with passed in UID', (done) => {
-      Connection.call.callsFake(() => {
-        console.log('YO YO YO2');
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess();
       });
-      // connection.del.callsFake(
-      //   (endpoint, data, params, onSuccess) => {
-      //     expect(endpoint).to.equal('/users');
-      //     expect(params).to.not.be.null;
-      //     expect(params.uid).to.equal(testUser.uid);
-      //     expect(params.token).to.equal(testToken.token);
-      //     expect(tokenManager.getToken).to.have.been.calledOnce;
-      //     expect(tokenManager.decode).to.not.have.been.called;
-      //     onSuccess();
-      //   });
+
       deleteUser(testUser.uid).then(() => {
         // expect(connection.del).to.have.been.calledOnce;
         done();
@@ -78,18 +59,10 @@ export default describe('Server API: User', () => {
       const fieldsToUpdate = {
         name: 'newName',
       };
-      Connection.call.callsFake(() => {
-        console.log('YO YO YO3');
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess();
       });
-      // connection.put.callsFake(
-      //   (endpoint, data, params, onSuccess) => {
-      //     expect(endpoint).to.equal('/users');
-      //     expect(params).to.not.be.null;
-      //     expect(params.uid).to.equal(testUser.uid);
-      //     expect(tokenManager.getToken).to.have.been.calledOnce;
-      //     expect(tokenManager.decode).to.not.have.been.called;
-      //     onSuccess(fieldsToUpdate);
-      //   });
+
       modifyUser(fieldsToUpdate, testUser.uid).then(() => {
         // expect(connection.put).to.have.been.calledOnce;
         done();
@@ -99,18 +72,9 @@ export default describe('Server API: User', () => {
 
   describe('#getUser', () => {
     it('should return the requested user with passed in UID', (done) => {
-      Connection.call.callsFake(() => {
-        console.log('YO YO YO4');
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess(testUser);
       });
-      // connection.get.callsFake(
-      //   (endpoint, data, params, onSuccess) => {
-      //     expect(endpoint).to.equal('/users');
-      //     expect(params).to.not.be.null;
-      //     expect(params.uid).to.equal(testUser.uid);
-      //     expect(tokenManager.getToken).to.have.been.calledOnce;
-      //     expect(tokenManager.decode).to.have.not.been.called;
-      //     onSuccess(testUser);
-      //   });
       getUser(testUser.uid).then((userData) => {
         // expect(connection.get).to.have.been.calledOnce;
         expect(userData).to.equal(testUser);
