4e82d26f4fca0d237a1e5b1bd13f5ac0120b5708
Alexander Clines <alexander.clines@ttu.edu>
2017-06-17 17:30:07 -0500

    [server][test] Refined auth tests and removed unused code

--- test/server/test-auth.js
+++ test/server/test-auth.js
@@ -1,10 +1,9 @@
 import sinon from 'sinon';
 import { store } from 'App.jsx';
 import { AuthStates } from 'redux/actions';
-import logger from 'logger/logger.js';
+// import logger from 'logger/logger.js';
 import { login, logout } from 'server/user-auth';
 import * as tokenManager from 'server/core/tokenmanager';
-// import * as connection from 'server/core/connection';
 import Connection from 'server/core/connection';
 import { user38257001 as testUser } from './core/users';
 import { testToken1 as testToken } from './core/tokens';
@@ -23,22 +22,7 @@ export default describe('Server API: Auth', () => {
     sandbox.stub(Connection.prototype, 'call');
     sandbox.stub(tokenManager, 'saveToken');
     sandbox.stub(tokenManager, 'removeToken');
-  //   sinon.stub(connection, 'post').callsFake((endpoint, data, onSuccess, onError) => {
-  //     if (endpoint === '/auth') {
-  //       onSuccess({
-  //         token: testToken.token,
-  //       });
-  //     } else {
-  //       onError(new Error('Test: Invalid post endpoint'));
-  //     }
-  //   });
-  //   sinon.stub(connection, 'del').callsFake((endpoint, data, onSuccess, onError) => {
-  //     if (endpoint === '/auth') {
-  //       onSuccess();
-  //     } else {
-  //       onError(new Error('Test: Invalid del endpoint'));
-  //     }
-  //   });
+    sandbox.stub(tokenManager, 'getToken');
   });
 
   afterEach(() => {
@@ -68,11 +52,15 @@ export default describe('Server API: Auth', () => {
         });
       });
       login(testUser.email, testUser.password).then((userData) => {
+        const connectionCallData = Connection.prototype.call.thisValues[0];
         expect(userData).to.not.be.null;
         expect(Connection.prototype.call).to.have.been.calledOnce;
-        // const connectionPostData = connection.post.getCall(0).args[1];
-        // expect(connectionPostData.email).to.equal(testUser.email);
-        // expect(connectionPostData.password).to.equal(testUser.password);
+        expect(connectionCallData.method).to.equal('post');
+        expect(connectionCallData.url).to.equal('/auth');
+        expect(connectionCallData.config.data).to.deep.equal({
+          email: testUser.email,
+          password: testUser.password,
+        });
         expect(tokenManager.saveToken).to.have.been.calledOnce;
         expect(tokenManager.saveToken.getCall(0).args[0]).to.equal(testToken.token);
         done();
@@ -82,23 +70,34 @@ export default describe('Server API: Auth', () => {
 
   describe('#logout', () => {
     it('should remove the session token', (done) => {
-      Connection.prototype.call.callsFake((onSuccess) => {
-        const method = Connection.prototype.call.thisValues[Connection.prototype.call.callCount-1].method;
+      tokenManager.getToken.callsFake(() => testToken.token);
+      Connection.prototype.call.callsFake((onSuccess, onError) => {
+        const method = Connection.prototype.call
+          .thisValues[Connection.prototype.call.callCount - 1].method;
         if (method === 'post') {
           onSuccess({
             token: testToken.token,
           });
-        } else {
+        } else if (method === 'delete') {
           onSuccess();
+        } else {
+          onError();
         }
       });
       login(testUser.email, testUser.password).then((userData) => {
         expect(userData).to.not.be.null;
         logout().then(() => {
           expect(Connection.prototype.call).to.have.been.calledTwice;
+          const connectionCallData = Connection.prototype.call.thisValues[1];
+          expect(connectionCallData.method).to.equal('delete');
+          expect(connectionCallData.url).to.equal('/auth');
+          expect(connectionCallData.config.data).to.deep.equal({
+            token: testToken.token,
+          });
           const reduxUser = store.getState().user;
           expect(reduxUser.role).to.equal(AuthStates.GUEST);
           expect(tokenManager.removeToken).to.have.been.calledOnce;
+          expect(tokenManager.getToken).to.have.been.calledOnce;
           done();
         }).catch((err) => { done(err); });
       }).catch((err) => { done(err); });
