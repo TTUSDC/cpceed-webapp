9d67f7266e1c8772ba5bfbfaa7b456a7ec11f1fc
Alexander Clines <alexander.clines@ttu.edu>
2017-06-19 19:06:18 -0500

    [server][docs] Added usage documentation for events

--- src/server/events.js
+++ src/server/events.js
@@ -5,6 +5,22 @@ import Connection from 'server/core/connection';
 /**
  * Creates a new event on the server
  *
+ * @example
+ * const newEvent = {
+ *  creator: '<User UID>',
+ *  title: 'Studying',
+ *  description: 'Studying for finals',
+ *  datetime: new Date(),
+ *  location: 'Library',
+ *  category: 'misc',
+ * };
+ *
+ * createEvent(newEvent).then((result) => {
+ *   logger.info(`Created new event with uid: ${result.uid}`);
+ * }).catch((err) => {
+ *   logger.error(err);
+ * });
+ *
  * @param {EventSchema} newEvent - The data for the new event
  * @returns {Promise<string, Error} - Resolves with UID of created event
  *                                    rejects with the error
@@ -23,6 +39,15 @@ export function createEvent(newEvent) {
 /**
  * Modifies an event on the server.
  * Must be the creator of the event or admin to modify.
+ *
+ * @example
+ * const updatedEvent.description = 'Giving up on finals';
+ *
+ * modifyEvent(updatedEvent).then((result) => {
+ *   logger.info(`Event with uid ${result.uid} was updated`);
+ * }).catch((err) => {
+ *   logger.error(err);
+ * });
  *
  * @param {string} uid - UID of event to be modified
  * @param {EventSchema} updatedEvent - Event data to be updated
@@ -44,6 +69,13 @@ export function modifyEvent(uid, updatedEvent) {
  * Removes an event on the server.
  * Must be the creator of the event or admin to remove
  *
+ * @example
+ * removeEvent(someEvent).then(() => {
+ *  logger.info(`Event with UID ${someEvent.uid} removed`);
+ * }).catch((err) => {
+ *  logger.error(err);
+ * });
+ *
  * @param {string} uid - UID of the event to remove
  * @returns {Promise<,Error>} - Resolves with nothing if successful
  */
@@ -60,6 +92,13 @@ export function removeEvent(uid) {
 
 /**
  * Retrieves an event from the server by its UID
+ *
+ * @example
+ * getEvent('12345').then((event) => {
+ *  logger.info(`Retrieved event with uid: ${event.uid}`); // == 12345
+ * }).catch((err) => {
+ *  logger.error(err);
+ * });
  *
  * @param {string} uid - UID of the event to be retrieved
  * @returns {Promise<EventSchema, Error} - Resolves with event info
@@ -78,6 +117,15 @@ export function getEvent(uid) {
 /**
  * Retrieves all events from the server.
  * In future, this will accept search and filter parameters.
+ *
+ * @example
+ * getAllEvents().then((events) => {
+ *  Object.keys(events).forEach((eventUid) => {
+ *    logger.info(`Retrieved event with UID: ${eventUid} and data: ${events[eventUid]}`);
+ *  });
+ * }).catch((err) => {
+ *  logger.error(err);
+ * });
  *
  * @returns {Promise<Object, Error} - Resolves with an
  *           object of key value pairs where the key is the Event UID and the
