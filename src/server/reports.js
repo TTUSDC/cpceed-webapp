3ca5443b74312f6fbe6c5ea3b7dd6d50dd599167
Alexander Clines <alexander.clines@ttu.edu>
2017-06-24 10:57:26 -0500

    [server] Added new CRUD methods for reports

--- src/server/reports.js
+++ src/server/reports.js
@@ -1,6 +1,64 @@
+// import logger from 'logger.js';
+import Connection from 'server/core/connection';
+
 import * as utils from './utils';
 import * as dummyData from '../../test/server/dummy-data';
 
+export function createReport(newReport) {
+  return new Promise((resolve, reject) => {
+    new Connection()
+      .post()
+      .reports()
+      .data(newReport)
+      .token()
+      .call(resolve, reject);
+  });
+}
+
+export function modifyReport(uid, updatedReport) {
+  return new Promise((resolve, reject) => {
+    new Connection()
+      .put()
+      .reports()
+      .data(updatedReport)
+      .params({ uid })
+      .token()
+      .call(resolve, reject);
+  });
+}
+
+export function removeReport(uid) {
+  return new Promise((resolve, reject) => {
+    new Connection()
+      .del()
+      .reports()
+      .params({ uid })
+      .token()
+      .call(resolve, reject);
+  });
+}
+
+export function getReport(uid) {
+  return new Promise((resolve, reject) => {
+    new Connection()
+      .get()
+      .reports()
+      .param({ uid })
+      .token()
+      .call(resolve, reject);
+  });
+}
+
+export function getAllReports() {
+  return new Promise((resolve, reject) => {
+    new Connection()
+      .get()
+      .all()
+      .reports()
+      .token()
+      .call(resolve, reject);
+  });
+}
 export function create(newReport) {
   return new Promise((resolve, reject) => {
     const uid = utils.getRandomString();
