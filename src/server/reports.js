914b938696fc88df40e495b09a5bcb4aa339cd0c
Alexander Clines <alexander.clines@ttu.edu>
2017-06-24 11:06:00 -0500

    [server] Removed old report code

--- src/server/reports.js
+++ src/server/reports.js
@@ -1,9 +1,6 @@
 // import logger from 'logger.js';
 import Connection from 'server/core/connection';
 
-import * as utils from './utils';
-import * as dummyData from '../../test/server/dummy-data';
-
 export function createReport(newReport) {
   return new Promise((resolve, reject) => {
     new Connection()
@@ -43,7 +40,7 @@ export function getReport(uid) {
     new Connection()
       .get()
       .reports()
-      .param({ uid })
+      .params({ uid })
       .token()
       .call(resolve, reject);
   });
@@ -57,38 +54,5 @@ export function getAllReports() {
       .reports()
       .token()
       .call(resolve, reject);
-  });
-}
-export function create(newReport) {
-  return new Promise((resolve, reject) => {
-    const uid = utils.getRandomString();
-    dummyData.reports[uid] = newReport;
-    resolve(uid);
-  });
-}
-
-export function modify(uid, updatedReport) {
-  return new Promise((resolve, reject) => {
-    dummyData.reports[uid] = updatedReport;
-    resolve();
-  });
-}
-
-export function remove(uid) {
-  return new Promise((resolve, reject) => {
-    dummyData.reports[uid] = undefined;
-    resolve();
-  });
-}
-
-export function getByUid(uid) {
-  return new Promise((resolve, reject) => {
-    resolve(dummyData.reports[uid]);
-  });
-}
-
-export function getAll() {
-  return new Promise((resolve, reject) => {
-    resolve(dummyData.reports);
   });
 }
