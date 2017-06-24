83f770c502679d4d5441087017c6d7ee799237cb
Alexander Clines <alexander.clines@ttu.edu>
2017-06-24 11:05:27 -0500

    [server][test] Added report testing

--- test/server/index.js
+++ test/server/index.js
@@ -2,3 +2,4 @@ import testAuth from './test-auth.js';
 import testCoreTokenManager from './test-core-tokenmanager.js';
 import testUser from './test-user.js';
 import testEvents from './test-events.js';
+import testReports from './test-reports.js';
--- test/server/test-reports.js
+++ test/server/test-reports.js
@@ -1,79 +1,142 @@
-var expect = require("chai").expect;
+import sinon from 'sinon';
+import * as tokenManager from 'server/core/tokenmanager';
+import Connection from 'server/core/connection';
+import {
+  createReport,
+  modifyReport,
+  removeReport,
+  getReport,
+  getAllReports,
+} from 'server/reports';
+// import logger from 'logger.js';
+import { user38257001 as testUser } from './core/users';
+import { testToken1 as testToken } from './core/tokens';
+import { generateOtherReportData } from '../core/reports';
 
-import server from '../../src/server/server'
+const sinonChai = require('sinon-chai');
+const chai = require('chai');
 
-export default describe('reports', function() {
-  var testEventReport = {
-    type: "event",
-    student: "1234567",
-    event: "9876543321"
-  }
-  var testEventReportUid = undefined;
-  describe('#createReport(newReport)', function() {
-      it('should create a new event report.', function(done) {
+chai.use(sinonChai);
+const expect = chai.expect;
+const sandbox = sinon.sandbox.create();
 
-        server.createReport(testEventReport).then(function(uid) {
-          expect(server.dummyData.reports[uid]).to.equal(testEventReport);
-          testEventReportUid = uid;
-          done();
-        }).catch(function(reason) {
-          done(reason)
-        })
+export default describe('Server API: Reports', () => {
+  beforeEach(() => {
+    sandbox.stub(Connection.prototype, 'call');
+    sandbox.stub(tokenManager, 'getToken').callsFake(() => testToken.token);
+    sandbox.spy(tokenManager, 'decode');
+  });
 
-      })
-    }),
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-    describe('#modifyReport(uid, updatedReport)', function() {
-      it('should modify a created event report', function(done) {
-        expect(testEventReportUid).to.not.be.undefined;
-        testEventReport.student = "98765432";
-        server.modifyReport(testEventReportUid, testEventReport).then(function() {
-          expect(server.dummyData.reports[testEventReportUid]).to.equal(testEventReport)
-          done();
-        }).catch(function(reason) {
-          done(reason)
-        })
-      })
-    }),
+  describe('#createReport', () => {
+    it('should create a new report', (done) => {
+      const testReport = generateOtherReportData(testUser.uid);
 
-    describe('#getReportByUid(uid)', function(){
-      it('should return the created test event report.', function(done){
-        expect(testEventReportUid).to.not.be.undefined;
-        server.getReportByUid(testEventReportUid).then(function(report){
-          expect(report).to.equal(server.dummyData.reports[testEventReportUid])
-          done()
-        }).catch(function(reason){
-          done(reason)
-        })
-      })
-    }),
+      const expectedResult = { uid: '12345678' };
 
-    describe('#getAllReports()', function(){
-      it('should get all reports.', function(done){
-        server.getAllReports().then(function(reports){
-          expect(reports).to.equal(server.dummyData.reports);
-          done();
-        }).catch(function(reason) {
-          done(reason);
-        })
-      })
-    }),
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess(expectedResult);
+      });
 
-    describe('#deleteReport(uid)', function() {
-      it('should delete a created event report.', function(done) {
-        expect(testEventReportUid).to.not.be.undefined;
-        server.deleteReport(testEventReportUid).then(function() {
-          expect(server.dummyData.reports[testEventReportUid]).to.be.undefined;
-          done();
-        }).catch(function(reason) {
-          done(reason);
-        })
-      })
-    })
+      createReport(testReport).then((data) => {
+        expect(Connection.prototype.call).to.have.been.calledOnce;
+        const connectionCallData = Connection.prototype.call.thisValues[0];
+        expect(connectionCallData.method).to.equal('post');
+        expect(connectionCallData.url).to.equal('/reports');
+        expect(connectionCallData.config.data).to.deep.equal(testReport);
+        expect(data).to.equal(expectedResult);
+        expect(connectionCallData.config.params.token).to.equal(testToken.token);
+        done();
+      }).catch(done);
+    });
+  });
+  describe('#modifyReport', () => {
+    it('should modify an existing report.', (done) => {
+      const testReport = generateOtherReportData(testUser.uid);
+      const testReportUid = '123kl4j123srfa3';
 
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess(testReport);
+      });
 
+      modifyReport(testReportUid, testReport).then((data) => {
+        expect(Connection.prototype.call).to.have.been.calledOnce;
+        const connectionCallData = Connection.prototype.call.thisValues[0];
+        expect(connectionCallData.method).to.equal('put');
+        expect(connectionCallData.url).to.equal('/reports');
+        expect(connectionCallData.config.data).to.deep.equal(testReport);
+        expect(connectionCallData.config.params.token).to.equal(testToken.token);
+        expect(connectionCallData.config.params.uid).to.equal(testReportUid);
+        expect(data).to.equal(testReport);
+        done();
+      }).catch(done);
+    });
+  });
+  describe('#removeReport', () => {
+    it('should remove an existing report.', (done) => {
+      const testReportUid = '123kl4j123srfa3';
 
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess();
+      });
 
+      removeReport(testReportUid).then(() => {
+        expect(Connection.prototype.call).to.have.been.calledOnce;
+        const connectionCallData = Connection.prototype.call.thisValues[0];
+        expect(connectionCallData.method).to.equal('delete');
+        expect(connectionCallData.url).to.equal('/reports');
+        expect(connectionCallData.config.params.uid).to.equal(testReportUid);
+        expect(connectionCallData.config.params.token).to.equal(testToken.token);
+        done();
+      }).catch(done);
+    });
+  });
+  describe('#getReport', () => {
+    it('should return an existing report by UID.', (done) => {
+      const testReport = generateOtherReportData(testUser.uid);
+      const testReportUid = '123kl4j123srfa3';
 
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess(testReport);
+      });
 
-})
+      getReport(testReportUid).then((data) => {
+        expect(Connection.prototype.call).to.have.been.calledOnce;
+        const connectionCallData = Connection.prototype.call.thisValues[0];
+        expect(connectionCallData.method).to.equal('get');
+        expect(connectionCallData.url).to.equal('/reports');
+        expect(connectionCallData.config.params.uid).to.equal(testReportUid);
+        expect(connectionCallData.config.params.token).to.equal(testToken.token);
+        expect(data).to.equal(testReport);
+        done();
+      }).catch(done);
+    });
+  });
+  describe('#getAllReports', () => {
+    it('should return all existing reports.', (done) => {
+      const testReports = {
+        id1: generateOtherReportData(testUser.uid),
+        id2: generateOtherReportData(testUser.uid),
+        id3: generateOtherReportData(testUser.uid),
+      };
+
+      Connection.prototype.call.callsFake((onSuccess) => {
+        onSuccess(testReports);
+      });
+
+      getAllReports().then((list) => {
+        expect(Connection.prototype.call).to.have.been.calledOnce;
+        const connectionCallData = Connection.prototype.call.thisValues[0];
+        expect(connectionCallData.method).to.equal('get');
+        expect(connectionCallData.url).to.equal('/reports');
+        expect(connectionCallData.endpoint).to.equal('all');
+        expect(connectionCallData.config.params.token).to.equal(testToken.token);
+        expect(list).to.deep.equal(testReports);
+        done();
+      }).catch(done);
+    });
+  });
+});
