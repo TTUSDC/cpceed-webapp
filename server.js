44d6ebd4b76385ff2a11ec53304ed8cb691403c7
NilsG-S <NilsG-S@users.noreply.github.com>
2017-08-16 21:00:56 -0500

    Setup Express server to support passport

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('api/passport-config.js');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const apiRouter = require('api/server');
@@ -12,43 +14,10 @@ const logger = require('api/core/logger');
 // load environment variables from .env file.
 dotenv.load({ path: process.env.ENV_PATH || '.env.default' });
 
-// Express server.
-const app = express();
-app.use(cors());
-
-// Express configuration.
+// Definitions
 const port = process.env.PORT || 3000;
-app.set('port', port);
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-// Connect to mongodb.
 const mongoURL = process.env.MONGODB_URI || process.env.MONGOLAB_URI;
-mongoose.Promise = global.Promise;
-mongoose.connection.on('error', (err) => {
-  logger.fatal(err);
-  logger.info('MongoDB connection error. Please make sure MongoDB is running.');
-  process.exit(1);
-});
-
-app.use('/', express.static(`${__dirname}/build`));
-app.use('/api', apiRouter);
-
-// 404.
-app.use((req, res, next) => {
-  const err = new Error('Route not found.');
-
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.json(err.message);
-});
-
+const app = express();
 let service;
 
 /**
@@ -59,7 +28,59 @@ let service;
  * @returns {http.Server|*} - The HTTP Server listening
  */
 const start = (cb) => {
+  // Connect to mongodb.
+  mongoose.Promise = global.Promise;
+  mongoose.connection.on('error', (err) => {
+    logger.fatal(err);
+    logger.info('MongoDB connection error. Please make sure MongoDB is running.');
+    process.exit(1);
+  });
   mongoose.connect(mongoURL);
+
+  // Express configuration.
+  app.use(cors());
+  app.set('port', port);
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  // TODO(NilsG-S): setup https for production
+  app.use(session({
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 14,
+      secure: process.env.NODE_ENV === 'production',
+    },
+    name: 'cpceed.sid',
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SECRET,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 * 14,
+      collection: 'sessions',
+      stringify: false,
+    }),
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Routes.
+  app.use('/', express.static(`${__dirname}/build`));
+  app.use('/api', apiRouter);
+
+  // 404.
+  app.use((req, res, next) => {
+    const err = new Error('Route not found.');
+
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.json(err.message);
+  });
+
   service = app.listen(port, cb);
   return service;
 };
