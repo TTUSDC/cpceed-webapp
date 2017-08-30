590c99f8e3fdfa058a7cf9258dbec85452b2a5f9
NilsG-S <NilsG-S@users.noreply.github.com>
2017-08-30 13:10:00 -0500

    Added documentation to connection.js

--- api/connection.js
+++ api/connection.js
@@ -4,6 +4,11 @@ const mockgoose = require('mockgoose');
 const mongoURL = process.env.MONGODB_URI || process.env.MONGOLAB_URI;
 mongoose.Promise = global.Promise;
 
+/**
+ * Opens a connection from mongoose to the database, wrapping it with
+ * mockgoose if `NODE_ENV` is 'test'.
+ * @returns {Promise<undefined, Error>} - Resolves when connection is opened.
+ */
 function open() {
   return new Promise((resolve, reject) => {
     if (process.env.NODE_ENV === 'test') {
@@ -21,6 +26,11 @@ function open() {
   });
 }
 
+/**
+ * Closes the connection from mongoose to the database, using `unmock` if
+ * NODE_ENV is 'test'.
+ * @returns {Promise<undefined, Error>} - Resolves when connection is closed.
+ */
 function close() {
   if (process.env.NODE_ENV === 'test') {
     return new Promise((resolve) => {
