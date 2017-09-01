d6595ca03adb738e25feca3fae0ff7666580c22f
NilsG-S <NilsG-S@users.noreply.github.com>
2017-09-01 14:13:40 -0500

    Added root level hooks for database connection

--- test/api/api-test-globals.js
+++ test/api/api-test-globals.js
@@ -6,13 +6,26 @@ const mockgoose = require('mockgoose');
 const mongoose = require('mongoose');
 const chai = require('chai');
 const sinon = require('sinon');
+const connection = require('api/connection.js');
+const app = require('api/app.js');
 
 chai.use(require('chai-shallow-deep-equal'));
 chai.use(require('chai-moment'));
 chai.use(require('sinon-chai'));
 
+global.app = app;
 global.mockgoose = mockgoose;
 global.mongoose = mongoose;
 global.sinon = sinon;
 global.expect = chai.expect;
 global.should = chai.should();
+
+// Connect to the database
+before((done) => {
+  connection.open().then(() => done()).catch(done);
+});
+
+// Disconnect from the database
+after((done) => {
+  connection.close().then(() => done()).catch(done);
+});
